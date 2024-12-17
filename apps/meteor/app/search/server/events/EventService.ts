5c6cfb441ad2e65873052b2e69a2fed2744a6b37
Saish Mungase <saishmungase@gmail.com>
2024-12-17 12:08:16 +0530

    Implemented Performant Cache In Search Event Service.

--- apps/meteor/app/search/server/events/EventService.ts
+++ apps/meteor/app/search/server/events/EventService.ts
@@ -2,9 +2,28 @@ import { SearchLogger } from '../logger/logger';
 import { searchProviderService } from '../service';
 
 export class EventService {
-	_pushError(name: string, value: string, _payload?: unknown) {
-		// TODO implement a (performant) cache
+
+	// TODO implement a (performant) cache => TODO Completed Here
+
+	private errorCache: Map<string, unknown> = new Map();
+
+	private _generateCacheKey(name: string, value: string): string {
+		return `${name}:${value}`;
+	}
+
+	private _pushError(name: string, value: string, _payload?: unknown) {
+
+		const key = this._generateCacheKey(name, value);
+
+		if (this.errorCache.has(key)) {
+			SearchLogger.debug(`Error '${key}' is already cached, skipping duplicate log.`);
+			return;
+		}
+
+		this.errorCache.set(key, _payload);
+
 		SearchLogger.debug(`Error on event '${name}' with id '${value}'`);
+
 	}
 
 	promoteEvent(name: string, value: string, payload?: unknown) {
@@ -13,3 +32,5 @@ export class EventService {
 		}
 	}
 }
+
+
